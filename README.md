1298cc70fd52f625def2679c7345d36203cf69ef
Samuel Rembisz <sjrembisz07@gmail.com>
2022-04-14 22:10:18 -0400

    Initialize with_postgres

---
+++ README.md
@@ -0,0 +1,1 @@
+# Rocket_Vue_Boilerplate
---
+++ src/main.rs
@@ -0,0 +1,67 @@
+#[macro_use]
+extern crate rocket;
+
+extern crate openssl;
+#[macro_use]
+extern crate diesel;
+
+#[macro_use]
+extern crate diesel_migrations;
+
+embed_migrations!("migrations");
+
+mod util;
+
+use diesel::prelude::*;
+use dotenv::dotenv;
+use rocket::{fs::NamedFile, routes};
+use std::{
+    env,
+    path::{Path, PathBuf},
+};
+
+#[get("/")]
+async fn index() -> Option<NamedFile> {
+    NamedFile::open(Path::new("static/index.html")).await.ok()
+}
+
+#[get("/<file..>")]
+async fn files(file: PathBuf) -> Option<NamedFile> {
+    match NamedFile::open(Path::new("static/").join(file)).await {
+        Ok(file) => Some(file),
+        Err(_) => NamedFile::open(Path::new("static/index.html")).await.ok(),
+    }
+}
+
+#[rocket::main]
+async fn main() {
+    dotenv().ok();
+    openssl_probe::init_ssl_cert_env_vars();
+
+    let connection = create_connection().expect("Failed to connect to database");
+
+    embedded_migrations::run(&connection).expect("Failed to run embedded migrations");
+
+    std::mem::drop(connection);
+
+    match rocket::build()
+        .mount("/", routes![index, files])
+        .attach(crate::util::CORS)
+        .launch()
+        .await
+    {
+        Ok(_) => {}
+        Err(e) => {
+            eprintln!("{}", e);
+            std::process::exit(1);
+        }
+    }
+}
+
+fn create_connection() -> Option<PgConnection> {
+    let database_url = unwrap_or_return!(env::var("DATABASE_URL"), "Database URL not set.");
+    Some(unwrap_or_return!(
+        PgConnection::establish(&database_url),
+        "Error connecting to database!"
+    ))
+}
