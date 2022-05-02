e61ae4b482f4184b470f29c0bd9e7bf5c6f114a7
Samuel Rembisz <sjrembisz07@gmail.com>
2022-05-02 01:20:17 -0400

    Got oauth working with Google

--- src/auth.rs
+++ src/auth.rs
@@ -25,29 +25,25 @@ pub async fn get_auth() -> Json<String> {
     let token_url = TokenUrl::new("https://www.googleapis.com/oauth2/v3/token".to_string())
         .expect("Invalid token endpoint URL");
 
-    // Set up the config for the Google OAuth2 process.
     let client = BasicClient::new(
         google_client_id,
         Some(google_client_secret),
         auth_url,
         Some(token_url),
     )
-    // This example will be running its own server at localhost:9999.
-    // See below for the server implementation.
     .set_redirect_uri(
-        RedirectUrl::new("https://vidmeste.hogbisz.com/me/s2".to_string())
-            .expect("Invalid redirect URL"),
+        RedirectUrl::new(
+            env::var("BASE_URL").expect("Missing the BASE_URL environment variable.") + "me/s2",
+        )
+        .expect("Invalid redirect URL"),
     )
-    // Google supports OAuth 2.0 Token Revocation (RFC-7009)
     .set_revocation_uri(
         RevocationUrl::new("https://oauth2.googleapis.com/revoke".to_string())
             .expect("Invalid revocation endpoint URL"),
     );
 
-    // Generate the authorization URL to which we'll redirect the user.
     let (authorize_url, csrf_state) = client
         .authorize_url(CsrfToken::new_random)
-        // This example is requesting access to the "calendar" features and the user's profile.
         .add_scope(Scope::new(
             "https://www.googleapis.com/auth/calendar".to_string(),
         ))
@@ -105,10 +101,11 @@ pub async fn do_work(code: String, state: String) -> Json<String> {
     let token_response = client
         .exchange_code(code)
         .set_redirect_uri(Cow::Borrowed(
-            &RedirectUrl::new("https://vidmeste.hogbisz.com/me/s2".to_string())
-                .expect("Invalid redirect URL"),
+            &RedirectUrl::new(
+                env::var("BASE_URL").expect("Missing the BASE_URL environment variable.") + "me/s2",
+            )
+            .expect("Invalid redirect URL"),
         ))
-        .add_extra_param("revocation", value)
         .request_async(async_http_client)
         .await;
 
@@ -124,11 +121,23 @@ pub async fn do_work(code: String, state: String) -> Json<String> {
         None => token_response.access_token().into(),
     };
 
-    client
+    match client
+        .set_revocation_uri(
+            RevocationUrl::new("https://oauth2.googleapis.com/revoke".to_string())
+                .expect("Invalid revocation endpoint URL"),
+        )
         .revoke_token(token_to_revoke)
         .unwrap()
         .request_async(async_http_client)
         .await
-        .expect("Failed to revoke token");
-    make_json_response!(200, "OK! Go back to your terminal :)")
+    {
+        Ok(_) => {
+            info!("Token revoked successfully");
+            make_json_response!(200, "OK! Go back to your terminal :)")
+        }
+        Err(e) => {
+            error!("Error revoking token: {}", e);
+            make_json_response!(500, "Internal Server Error")
+        }
+    }
 }
